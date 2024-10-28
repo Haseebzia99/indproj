ee2730d1d60bab0d7996a6ec918b1c3002d1a814
haseeb <haseeb.z@raytheon.co.uk>
2024-10-28 11:10:40 +0000

    font chang

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   // output: "export",
-  // basePath: "/indproj",
+  basePath: "/indproj",
   // // robotsTxtOptions: {
   // //   policies: [{ userAgent: "*" }],
   // // },
